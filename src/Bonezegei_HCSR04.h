b8517b42a19a5cb27b2b72a7e22df1ed25efd897
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2023-07-30 12:36:13 +0800

    Update Bonezegei_HCSR04.h

--- src/Bonezegei_HCSR04.h
+++ src/Bonezegei_HCSR04.h
@@ -1,1 +1,28 @@
+/*
+  This Library is written for HCSR04 ultrasonic Module
+  Author: Bonezegei (Jofel Batutay)
+  Date: July 2023 
+  
+*/
+#ifndef _BONEZEGEI_HCSR04_H_
+#define _BONEZEGEI_HCSR04_H_
 
+
+
+class Bonezegei_HCSR04{
+  public:
+  Bonezegei_HCSR04();
+  Bonezegei_HCSR04(int trigger, int echo);
+
+  void begin();
+
+  int getDistance();
+
+  int _trigger; 
+  int _echo;
+  int maxDistance;
+  int timeout;
+};
+
+
+#endif
